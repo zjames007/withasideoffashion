9aa3573e906fbcf0ecb64e7ec9f2ff944d4b10c3
Zoe James <zjames@Zoes-MacBook-Air.local>
2019-10-24 16:00:44 -0500

    add uncode theme

---
+++ wp-content/themes/uncode/woocommerce/notices/success.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Show messages
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! $messages ){
+	return;
+}
+
+$class = is_product() ? ' limit-width double-top-padding no-bottom-padding' : ' no-h-padding no-top-padding double-bottom-padding';
+
+?>
+
+<?php foreach ( $messages as $message ) :
+	$message = str_replace('button wc-forward', 'button wc-forward btn-link', $message);
+	?>
+	<div class="row-container row-message">
+		<div class="row-parent<?php echo esc_attr($class); ?>">
+			<div class="woocommerce-message woocomerce-success" role="alert"><?php echo wp_kses_post( $message ); ?></div>
+		</div>
+	</div>
+<?php endforeach; ?>
